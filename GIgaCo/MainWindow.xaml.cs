b503425da9f826c9eaf60ad4a730137336cb8491
Matvey <97594483+Avar1tia@users.noreply.github.com>
2022-11-27 21:43:17 +0300

    Add files via upload

---
+++ GIgaCo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GIgaCo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void gg(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new otrasl());
+        }
+
+
+        private void siz(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new siz());
+        }
+
+    }
+}
